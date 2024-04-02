436a6ae3a7a1c1132b328858e3e8407b60de5842
Hardik Gupta <113587014+CodingWithHardik@users.noreply.github.com>
2024-04-02 20:19:38 +0530

    Create form-2.sh

---
+++ ARC113/form-2.sh
@@ -0,0 +1,32 @@
+cat > city_temp_schema.avsc << EOF
+{                                             
+            "type" : "record",                               
+            "name" : "Avro",                                 
+            "fields" : [                                     
+            {                                                
+                "name" : "city",                             
+                "type" : "string"                            
+            },                                               
+            {                                                
+                "name" : "temperature",                      
+                "type" : "double"                            
+            },                                               
+            {                                                
+                "name" : "pressure",                         
+                "type" : "int"                               
+            },                                               
+            {                                                
+                "name" : "time_position",                    
+                "type" : "string"                            
+            }                                                
+        ]                                                    
+    }
+EOF
+gcloud pubsub schemas create city-temp-schema \
+--type=AVRO \
+--definition-file=city_temp_schema.avsc
+gcloud pubsub topics create temp-topic \
+--schema=temperature-schema \
+--message-encoding=JSON 
+git clone https://github.com/CodingWithHardik/ARC113.git
+cd ARC113/helloPubSub
