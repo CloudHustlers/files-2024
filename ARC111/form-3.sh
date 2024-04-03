b0dae0d17e248219035bd70f4f4b4d5de2973b04
Hardik Gupta <113587014+CodingWithHardik@users.noreply.github.com>
2024-04-04 00:21:08 +0530

    Update form-3.sh

--- ARC111/form-3.sh
+++ ARC111/form-3.sh
@@ -1,5 +1,5 @@
 gcloud config set compute/region $REGION
-gsutil mb -c COLDLINE gs://qwiklabs-gcp-02-69347bf5e399-bucket
+gsutil mb -c COLDLINE gs://$DEVSHELL_PROJECT_ID-bucket
 echo "This is an example of editing the file content for cloud storage object" > sample.txt
-gsutil cp sample.txt gs://qwiklabs-gcp-02-69347bf5e399-gcs-bucket/
-gcloud storage buckets update gs://qwiklabs-gcp-02-69347bf5e399-bucket-ops --default-storage-class=ARCHIVE
+gsutil cp sample.txt gs://$DEVSHELL_PROJECT_ID-gcs-bucket/
+gcloud storage buckets update gs://$DEVSHELL_PROJECT_ID-bucket-ops --default-storage-class=ARCHIVE
