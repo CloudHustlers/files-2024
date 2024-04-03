48085d3fc5906fed336fcee6f0fe00e849eb7de8
Hardik Gupta <113587014+CodingWithHardik@users.noreply.github.com>
2024-04-04 01:53:28 +0530

    Update form-1.sh

--- ARC111/form-1.sh
+++ ARC111/form-1.sh
@@ -1,4 +1,3 @@
-gcloud config set compute/region $REGION
 gsutil mb gs://$DEVSHELL_PROJECT_ID-bucket
 gsutil retention set 30s "gs://$DEVSHELL_PROJECT_ID-gcs-bucket"
 echo "CloudHustlers" > developers.txt
