a375648dd53b28c9c75d78fb2e8d742b5e1d8f41
Hardik Gupta <113587014+CodingWithHardik@users.noreply.github.com>
2024-04-04 01:53:47 +0530

    Update form-3.sh

--- ARC111/form-3.sh
+++ ARC111/form-3.sh
@@ -1,4 +1,3 @@
-gcloud config set compute/region $REGION
 gsutil mb -c COLDLINE gs://$DEVSHELL_PROJECT_ID-bucket
 echo "This is an example of editing the file content for cloud storage object" > sample.txt
 gsutil cp sample.txt gs://$DEVSHELL_PROJECT_ID-gcs-bucket/
